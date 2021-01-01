6abb1efca629cd27c33c393d661023192bd53466
luciorox11@outlook.com <luciodevinfo@gmail.com>
2020-12-31 22:27:03 -0300

    improving logic

--- src/helpers/remove-olds.js
+++ src/helpers/remove-olds.js
@@ -1,32 +1,35 @@
 const { readdirSync, statSync, unlinkSync } = require('fs');
 const { join } = require('path');
+const { EventEmitter } = require('events');
 const ms = require('ms');
 const { logger } = require('../middlewares/logs');
 require('dotenv').config();
 
 /**
  * Delete files that are longer than 1 minute
- * @function removeOldFiles
+ * @class removeOldFiles
  */
 
-const removeOldFiles = () => {
-  try {
-    const files = readdirSync(process.env.NEW_FILES);
-    files.forEach((file) => {
+class Remove extends EventEmitter {
+  async removeOldFiles() {
+    try {
+      const files = readdirSync(process.env.NEW_FILES);
+      files.forEach((file) => {
+        const stats = statSync(join(process.env.NEW_FILES, file));
+        const now = new Date().getTime();
 
-      const stats = statSync(join(process.env.NEW_FILES, file));
-      const now = new Date().getTime();
+        const endTime = new Date(stats.ctime).getTime() + ms('24h');
 
-      const endTime = new Date(stats.ctime).getTime() + ms('2s');
+        if (now > endTime) {
+          unlinkSync(join(process.env.NEW_FILES, file));
+          logger.info(`Remove - ${file}`);
+        }
 
-      if (now > endTime) {
-        unlinkSync(join(process.env.NEW_FILES, file));
-      }
-      
-    });
-  } catch (err) {
-    return logger.error(`Delete Old Files failed: ${err}`);
+      });
+    } catch (err) {
+      return logger.error(`Delete Old Files failed: ${err}`);
+    }
   }
-};
+}
 
-module.exports = removeOldFiles;
+module.exports = Remove;
